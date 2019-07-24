15adae82b1d49a1ec88fb91a7f1d7ff557619fa0
Gabriel Miguel <gmiguel14@gmail.com>
2019-07-24 11:05:23 -0300

    Creates js exercises

--- resolucoes/01-js-variaveis.js
+++ resolucoes/01-js-variaveis.js
@@ -1,19 +1,15 @@
 // EXERCÍCIO 01
 // Crie um vetor que possui o nome dos seus 5 artistas favoritos. Imprima no console o nome do 1o e do 3o artista da lista.
-
 // Ex: Michael Jackson Billy Corgan
 
 let artistas = ["Michael Jackson", "Billy Corgan", "Brad Pitt", "Jared Leto", "Nicki Minaj"];
 
 console.log(artistas[0], artistas[2])
 
-
 // EXERCÍCIO 02
 // Crie um objeto que represente seu personagem favorito. Use atributos como nome, idade, altura, série/filme/quadrinho/gameque ele pertence, habilidade, etc.
 // Imprima no console uma frase usando alguns de seus atributos.
-
 // Ex: Meu personagem favorito é o Goku e ele é um Sayajin.
-
 
 let personagem = {
   nome: "Goku",
