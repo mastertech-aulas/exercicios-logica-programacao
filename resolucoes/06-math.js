39e69df546d860640e396ac686bcf1e7f1cd4957
Lucas Melo <luucasmell@gmail.com>
2019-08-05 10:52:18 -0300

    Add first  for exercises solution

--- resolucoes/06-math.js
+++ resolucoes/06-math.js
@@ -17,3 +17,31 @@ function verificarSorteio(){
 }
 
 verificarSorteio();
+
+
+//  EXERCÍCIO 02 
+// Crie um programa que simule um ataque do meuPokemon contra o pokemonInimigo.
+// - O ataque de um pokemon pode causar de 1 à 100 pontos de dano. Caso o elemento do meuPokemon seja forte contra o elemento do pokemonInimigo,
+// o ataque recebe um bônus de 25%.
+
+// Use a seguinte referência
+// - Raio é forte contra água
+// - Água é forte contra fogo
+// - Fogo é forte contra planta
+// - Planta é forte contra raio
+
+// O programa deve determinar quanto foi o dano causado e se o pokemon inimigo foi derrotado (hp 0 ou menor)
+
+// `let tipos = ['raio', 'água', 'fogo', 'planta'];`
+
+// `let meuPokemon = {`
+//   `nome: 'Pikachu',`
+//   `tipo: tipos[0],`
+//   `hp: 100`
+// `}`
+
+// `let pokemonInimigo = {`
+//   `nome: 'Charmander',`
+//   `tipo: tipos[2],`
+//   `hp: 100`
+// `}`
